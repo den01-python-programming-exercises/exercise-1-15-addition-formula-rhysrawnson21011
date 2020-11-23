241a53645612cb71422b4f932f73dea4b578c72d
rhysrawnson2101 <Rhysrawnson@gmail.com>
2020-11-23 14:01:14 +0000

    added multiple number outputs

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,27 @@
+# I've tidied up the indents - they're really important to watch!
+
 def main():
     #write your code below this line
+  while True:
+    try:
+      number_one = int(input('please  choose a number: '))
+      break
+    except:
+        print("Please enter an integer!")
 
+  # watch indents...
+  while True:
+    try:
+      number_two = int(input('please choose a number: '))
+      break
+    except:
+        print("Please enter an integer!")
+             
+         
+  # The only thing you need to do now is fix this print statement so it matches the README. Something like this:
+  # print(str(number_one) + " + " + str(number_two) " = " number_one + number_two)
+  print(str(number_one) + ' + ' + str(number_two) + ' = ' +  str(number_one + number_two))
+  
+    
 if __name__ == '__main__':
-    main()
+    main() #this needs an indent
